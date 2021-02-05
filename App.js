d0b753044a40719f9cb52963baa192aa43c33d51
wb-zhouteng <zhouteng@yodinfo.com>
2021-02-05 16:48:31 +0800

    添加底部导航栏

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React, { Component, useState } from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator'
 
-const App = () => {
+const App = (props) => {
+
+  const [selectedTabItem, setSelectedTab] = useState('tb_msg')
+
+  const tabNavigatorItems = (selectedTab, title, icon, selectedIcon, imageStyle, mark, viewContent) => {
+    return (
+      <TabNavigator.Item
+        selected={selectedTabItem === selectedTab}
+        title={title}
+        renderIcon={() => <Image style={styles.myImage} source={icon} />}
+        renderSelectedIcon={() => <Image style={[styles.myImage, { tintColor: imageStyle }]} source={selectedIcon} />}
+        badgeText={mark}
+        onPress={() => { setSelectedTab(selectedTab) }}>
+        <View style={{ flex: 1 }}><Text>{viewContent}</Text></View>
+      </TabNavigator.Item>
+    )
+  }
+
   return (
-    <View></View>
-  );
-};
+    <View style={styles.container}>
+      <TabNavigator>
+        {tabNavigatorItems('tb_msg', "消息", null, null, '#ffe09a', "", "消息页面内容")}
+        {tabNavigatorItems('tb_contacts', "联系人", null, null, '#65bb74', "", "联系人页面内容")}
+        {tabNavigatorItems('tb_watch', "看点", null, null, '#6ebef3', "", "看点页面内容")}
+        {tabNavigatorItems('tb_dynamic', "动态", null, null, '#622193', "", "动态页面内容")}
+      </TabNavigator>
+    </View>
+  )
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  myImage: {
+    width: 22,
+    height: 22,
+  }
+});
+
+export default App
--- index.js
+++ index.js
@@ -2,8 +2,47 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, StyleSheet, View, Text, Image } from 'react-native';
+import React, { useState } from 'react'
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { defaultTab } from './AppMainTab.json';
+import TabNavigator from 'react-native-tab-navigator'
 
-AppRegistry.registerComponent(appName, () => App);
+
+const MainAppRouter = () => {
+  const [selectedTabItem, setSelectedItem] = useState(defaultTab)
+  tabNavigatorItems = (selectedTab, title, icon, selectedIcon, mark, viewContent) => {
+    return (
+      <TabNavigator.Item
+        selected={this.state.selectedTab === selectedTab}
+        title={title}
+        renderIcon={() => <Image style={styles.myImage} source={icon} />}
+        renderSelectedIcon={() => <Image style={styles.myImage} source={selectedIcon} />}
+        badgeText={mark}
+        onPress={() => this.setState({ selectedTab: selectedTab })}>
+        <View style={{ flex: 1 }}><Text>{viewContent}</Text></View>
+      </TabNavigator.Item>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <TabNavigator>
+        {this.tabNavigatorItems('***1', "消息", require('./src/img/icon_male.png'), require('./src/img/icon_male_selected.png'), "1", "消息页面内容")}
+        {this.tabNavigatorItems('***2', "联系人", require('./src/img/icon_female.png'), require('./src/img/icon_female_selected.png'), "", "联系人页面内容")}
+        {this.tabNavigatorItems('***3', "看点", require('./src/img/icon_record.png'), require('./src/img/icon_record_selected.png'), "", "看点页面内容")}
+        {this.tabNavigatorItems('***4', "动态", require('./src/img/icon_pic.png'), require('./src/img/icon_pic_selected.png'), "", "动态页面内容")}
+      </TabNavigator>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+AppRegistry.registerComponent(appName, () => MainAppRouter);
